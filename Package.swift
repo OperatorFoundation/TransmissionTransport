2d08a3501acdf0c5b87ee103950b3ded4b6207c2
CryptoSax <joshua@operatorfoundation.org>
2021-09-13 14:39:13 -0500

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -22,7 +22,7 @@ let package = Package(
         .package(url: "https://github.com/OperatorFoundation/Transport.git", from: "2.3.5"),
         .package(url: "https://github.com/OperatorFoundation/Transmission.git", from: "0.2.3"),
         .package(url: "https://github.com/OperatorFoundation/TransmissionLinux.git", from: "0.3.1"),
-        .package(url: "https://github.com/OperatorFoundation/NetworkLinux.git", from: "0.4.0"),
+        .package(url: "https://github.com/OperatorFoundation/NetworkLinux.git", from: "0.4.1"),
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
