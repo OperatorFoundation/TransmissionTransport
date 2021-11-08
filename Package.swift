7863458f21fb7133498b86a70f693d9f1fa6bcf4
CryptoSax <joshua@operatorfoundation.org>
2021-11-08 14:27:46 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -20,7 +20,7 @@ let package = Package(
         .package(url: "https://github.com/OperatorFoundation/Chord.git", from: "0.0.14"),
         .package(url: "https://github.com/OperatorFoundation/Datable.git", from: "3.1.1"),
         .package(url: "https://github.com/OperatorFoundation/Transport.git", from: "2.3.8"),
-        .package(url: "https://github.com/OperatorFoundation/Transmission.git", from: "1.0.2"),
+        .package(url: "https://github.com/OperatorFoundation/Transmission.git", from: "1.0.3"),
         .package(url: "https://github.com/OperatorFoundation/Net.git", from: "0.0.1")
     ],
     targets: [
