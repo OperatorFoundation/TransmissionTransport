2826b33a3d7cb28f10d3d13e5e4fd6bbba3cc747
CryptoSax <joshua@operatorfoundation.org>
2021-11-19 15:42:17 -0600

    Update Package.swift

--- Package.swift
+++ Package.swift
@@ -18,10 +18,10 @@ let package = Package(
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
         .package(url: "https://github.com/OperatorFoundation/Chord.git", from: "0.0.15"),
-        .package(url: "https://github.com/OperatorFoundation/Datable.git", from: "3.1.1"),
-        .package(url: "https://github.com/OperatorFoundation/Transport.git", from: "2.3.9"),
-        .package(url: "https://github.com/OperatorFoundation/Transmission.git", from: "1.2.0"),
-        .package(url: "https://github.com/OperatorFoundation/Net.git", from: "0.0.1")
+        .package(url: "https://github.com/OperatorFoundation/Datable.git", from: "3.1.2"),
+        .package(url: "https://github.com/OperatorFoundation/Transport.git", from: "2.3.10"),
+        .package(url: "https://github.com/OperatorFoundation/Transmission.git", from: "1.2.1"),
+        .package(url: "https://github.com/OperatorFoundation/Net.git", from: "1.0.0")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
