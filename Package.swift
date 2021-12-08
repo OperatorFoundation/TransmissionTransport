2d86a6a976d44646dab1ce200eb8b3449f293473
Dr. Brandon Wiley <brandon@operatorfoundation.org>
2021-12-08 10:47:05 -0600

    Switch dependencies to tracking branches

--- Package.swift
+++ Package.swift
@@ -17,11 +17,11 @@ let package = Package(
     dependencies: [
         // Dependencies declare other packages that this package depends on.
         // .package(url: /* package url */, from: "1.0.0"),
-        .package(url: "https://github.com/OperatorFoundation/Chord.git", from: "0.0.15"),
-        .package(url: "https://github.com/OperatorFoundation/Datable.git", from: "3.1.1"),
+        .package(url: "https://github.com/OperatorFoundation/Chord.git", branch: "main"),
+        .package(url: "https://github.com/OperatorFoundation/Datable.git", branch: "main"),
         .package(url: "https://github.com/OperatorFoundation/Transport.git", from: "2.3.9"),
-        .package(url: "https://github.com/OperatorFoundation/Transmission.git", from: "1.0.4"),
-        .package(url: "https://github.com/OperatorFoundation/Net.git", from: "0.0.1")
+        .package(url: "https://github.com/OperatorFoundation/Transmission.git", branch: "main"),
+        .package(url: "https://github.com/OperatorFoundation/Net.git", branch: "main")
     ],
     targets: [
         // Targets are the basic building blocks of a package. A target can define a module or a test suite.
